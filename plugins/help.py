d7ed8d4d92bf2e6b3537cbda803ff44564cf6032
seyidliadem <71027914+seyidliadem@users.noreply.github.com>
2021-04-05 00:46:51 +0400

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -3,5 +3,5 @@ from pyrogram import Client, Filters
 
 @Client.on_message(Filters.command(["help"]))
 async def start(client, message):
-    helptxt = f"Youtube Music və yaxud vide yükləmək isdəyirsinizsə mənə yükləmək isdədiyiniz videonun youtube linkini yollayın 😍"
+    helptxt = f"Youtube Music və yaxud vide yükləmək isdəyirsinizsə mənə yükləmək isdədiyiniz videonun youtube linkini yollayın 😍 Məs : https://youtu.be/BaiBFQh4m1s . Əlavə sualınız olsa @SeyidliAdem profilinə yazın "
     await message.reply_text(helptxt)
